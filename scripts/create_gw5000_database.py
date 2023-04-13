561b5b48d7d5579dd93a3479295197a54839e724
vandan-revanur <vandanrevanur@gmail.com>
2023-04-13 10:02:10 +0200

    Added a static atomic number map. 47x speed improvement

--- scripts/create_gw5000_database.py
+++ scripts/create_gw5000_database.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import re
-from mendeleev import element
 from collections import Counter
 from tqdm import tqdm
 from dateutil import tz
@@ -43,6 +42,19 @@ def calculate_n_electrons_in_mol(xyz):
     # corner case: C20H19AsCl2P
 
     n_electrons = 0
+
+    atom_number_map = {'H': 1, 'He': 2, 'Li': 3, 'Be': 4, 'B': 5, 'C': 6, 'N': 7, 'O': 8, 'F': 9, 'Ne': 10, 'Na': 11,
+    'Mg': 12, 'Al': 13, 'Si': 14, 'P': 15, 'S': 16, 'Cl': 17, 'Ar': 18, 'K': 19, 'Ca': 20, 'Sc': 21, 'Ti': 22,
+    'V': 23, 'Cr': 24, 'Mn': 25,'Fe': 26, 'Co': 27, 'Ni': 28, 'Cu': 29, 'Zn': 30, 'Ga': 31, 'Ge': 32, 'As': 33,
+    'Se': 34, 'Br': 35, 'Kr': 36, 'Rb': 37, 'Sr': 38, 'Y': 39, 'Zr': 40, 'Nb': 41, 'Mo': 42, 'Tc': 43, 'Ru': 44,
+    'Rh': 45, 'Pd': 46, 'Ag': 47, 'Cd': 48, 'In': 49, 'Sn': 50, 'Sb': 51, 'Te': 52, 'I': 53, 'Xe': 54, 'Cs': 55,
+    'Ba': 56, 'La': 57, 'Ce': 58, 'Pr': 59, 'Nd': 60, 'Pm': 61, 'Sm': 62, 'Eu': 63, 'Gd': 64, 'Tb': 65, 'Dy': 66,
+    'Ho': 67, 'Er': 68, 'Tm': 69, 'Yb': 70, 'Lu': 71, 'Hf': 72, 'Ta': 73, 'W': 74, 'Re': 75, 'Os': 76, 'Ir': 77,
+    'Pt': 78, 'Au': 79, 'Hg': 80, 'Tl': 81, 'Pb': 82, 'Bi': 83, 'Po': 84, 'At': 85, 'Rn': 86, 'Fr': 87, 'Ra': 88,
+    'Ac': 89, 'Th': 90, 'Pa': 91,'U': 92, 'Np': 93, 'Pu': 94, 'Am': 95, 'Cm': 96, 'Bk': 97, 'Cf': 98, 'Es': 99,
+    'Fm': 100, 'Md': 101, 'No': 102, 'Lr': 103, 'Rf': 104, 'Db': 105, 'Sg': 106, 'Bh': 107, 'Hs': 108, 'Mt': 109,
+    'Ds': 110, 'Rg': 111, 'Cn': 112, 'Nh': 113, 'Fl': 114, 'Mc': 115, 'Lv': 116, 'Ts': 117, 'Og': 118}
+
     num_regex = "[0-9]+"
     pos_of_numbers = [
         sub_str.start() for sub_str in re.finditer(num_regex, molecular_formula)
@@ -55,8 +67,7 @@ def calculate_n_electrons_in_mol(xyz):
     for pos_atom_symbol, pos_n_atoms in zip(pos_of_atomic_symbols, pos_of_numbers):
         atom_symbol = molecular_formula[pos_atom_symbol:pos_n_atoms]
         n_atoms = int(molecular_formula[pos_n_atoms])
-        atom = element(atom_symbol)
-        atom_number = atom.atomic_number
+        atom_number = atom_number_map[atom_symbol]
         n_electrons += atom_number*n_atoms
 
     return n_electrons
